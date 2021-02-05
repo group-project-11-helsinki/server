2547c4327020bab9e5eb1352fb0a42ca93cace67
hamzahay <hamzah.ay124@gmail.com>
2021-02-05 07:48:57 +0700

    working server gAuth

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const UserController = require('../controller/userController')
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+router.post('/googleLogin', UserController.googleLogin)
 
 module.exports = router
